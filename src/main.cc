8c084f66476d1422d57ff18aeb354a66e3ecc3e3
rmdashrf <attrix415@gmail.com>
2015-01-11 09:03:32 -0800

    Added payload size main test

--- src/main.cc
+++ src/main.cc
@@ -6,12 +6,15 @@
 #include <boost/algorithm/hex.hpp>
 #include <array>
 #include <deque>
+#include <mutex>
+#include <thread>
 
 #include "nntp/connection_info.hpp"
 #include "nntp/connection.hpp"
 #include "nntp/message.hpp"
 
 
+std::mutex qm;
 std::string get_run_nonce()
 {
     std::string ret;
@@ -33,13 +36,35 @@ std::string get_subject(const std::string& run_nonce, int count, int total)
     return stream.str();
 }
 
-p2u::nntp::message_payload get_body(const std::string& run_nonce, int count, int total)
+const char* LINE = "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP\r\n";
+
+p2u::nntp::message_payload get_body(const std::string& run_nonce, int count, int total, size_t payload_size)
 {
     p2u::nntp::message_payload ret;
-    std::ostringstream stream;
+    std::string content;
+    if (payload_size == 0)
+    {
+        content.reserve(160);
+    } else
+    {
+        content.reserve(payload_size + 50);
+    }
+
+    std::ostringstream stream{content};
+
     stream << "Testing posting\r\n"
            << "Run: " << count << "/" << total << "\r\n"
-           << "Nonce: " << run_nonce;
+           << "Nonce: " << run_nonce << "\r\n";
+
+
+    if (payload_size != 0)
+    {
+        while (stream.str().size() < payload_size)
+        {
+            stream << LINE;
+        }
+    }
+
     const auto& body = stream.str();
     ret = p2u::nntp::message_payload(body.begin(), body.end());
     return ret;
@@ -51,6 +76,8 @@ void post_next_article(p2u::nntp::connection* conn,
     std::cout << "[entering post_next_article]" << std::endl;
     std::cout << "Post next article on connection: " << std::hex << conn << std::endl;
     std::cout << "articles size: " << articles.size() << std::endl;
+
+    std::unique_lock<std::mutex> lock{qm};
     if (articles.size() < 1)
     {
         std::cout << "No more articles to post. calling close" << std::endl;
@@ -61,6 +88,7 @@ void post_next_article(p2u::nntp::connection* conn,
 
     auto article = articles.front();
     articles.pop_front();
+    lock.unlock();
     std::cout << "Trying to post article: " << article->article_header.subject << std::endl;
 
     conn->get_io_service().post([conn, &articles, article]() {
@@ -89,9 +117,10 @@ void post_next_article(p2u::nntp::connection* conn,
 
 int main(int argc, const char* argv[])
 {
+    std::ios_base::sync_with_stdio(false);
     if (argc < 6)
     {
-        std::cout << "Usage: " << argv[0] << " address port username password usetls" << std::endl;
+        std::cout << "Usage: " << argv[0] << " address port username password usetls num_connections num_articles payload_size" << std::endl;
         return 1;
     }
 
@@ -120,6 +149,10 @@ int main(int argc, const char* argv[])
     conn_info.port = static_cast<std::uint16_t>(std::stoul(argv[2]));
     conn_info.tls = std::stol(argv[5]) > 0;
 
+    size_t payload_size = argc >= 9 ? std::stoul(argv[8]) : 0;
+
+    std::cout << "Payload size is: " << payload_size << std::endl;
+
     if (conn_info.tls)
     {
         std::cout << "Using tls!" << std::endl;
@@ -129,18 +162,21 @@ int main(int argc, const char* argv[])
     std::vector<std::unique_ptr<p2u::nntp::connection>> connections;
     std::deque<std::shared_ptr<p2u::nntp::article>> articles;
 
-    const int NUM_ARTICLES = 80;
-    const int NUM_CONNECTIONS = 15;
+    int num_articles = argc >= 8 ? std::stoi(argv[7]) : 100;
+    int num_connections = argc >= 7 ? std::stoi(argv[6]) : 15;
 
-    for (int i = 0; i < NUM_ARTICLES; ++i)
+    size_t total_payload_bytes = 0;
+
+    for (int i = 0; i < num_articles; ++i)
     {
         auto article = std::make_shared<p2u::nntp::article>();
-        article->article_header = {"newsposter@tester.com", {"misc.test"}, get_subject(run_nonce, i+1, NUM_ARTICLES), {}};
-        article->article_payload = get_body(run_nonce, i+1, NUM_ARTICLES);
+        article->article_header = {"newsposter@tester.com", {"misc.test"}, get_subject(run_nonce, i+1, num_articles), {}};
+        article->article_payload = get_body(run_nonce, i+1, num_articles, payload_size);
         articles.push_back(article);
+        total_payload_bytes += article->article_payload.size();
     }
 
-    for (int i = 0; i < NUM_CONNECTIONS; ++i)
+    for (int i = 0; i < num_connections; ++i)
     {
         connections.emplace_back(new p2u::nntp::connection(io_service, conn_info));
         const auto conn = connections.back().get();
@@ -167,6 +203,28 @@ int main(int argc, const char* argv[])
             };
 
 //    stats_printer.async_wait(timercb);
+
+ //   const int NUM_THREADS = 4;
+ //   std::vector<std::thread> threads;
+ //   for (int i = 0; i < NUM_THREADS; ++i)
+ //   {
+ //       std::cout << "Spawning thread " << i << std::endl;
+ //       threads.emplace_back([&io_service](){ io_service.run();});
+ //   }
+
+ //   for (int i = 0; i < NUM_THREADS; ++i)
+ //   {
+ //       std::cout << "Joining thread " << i << std::endl;
+ //       threads[i].join();
+ //   }
+
+    auto start = std::chrono::high_resolution_clock::now();
     io_service.run();
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto elapsed = end - start;
+    auto seconds = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count() / 1000.0;
+    auto avg_speed = total_payload_bytes / seconds;
+    std::cout << "Finished sending " << std::dec << total_payload_bytes << " in " << seconds << " seconds for an average speed of " << avg_speed/1024.0 << " KB/s" << std::endl;
     return 0;
 }
